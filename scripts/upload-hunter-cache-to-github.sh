ead9d4f309b51d84514604f481e7e9b1342ff157
Paweł Bylica <chfast@gmail.com>
2017-07-24 23:04:52 +0200

    Test all

--- scripts/upload-hunter-cache-to-github.sh
+++ scripts/upload-hunter-cache-to-github.sh
@@ -3,6 +3,12 @@
 hunter_upload_script_url='https://raw.githubusercontent.com/ruslo/hunter/86b488030412a12c9a950977bf3d83b6fbcb7f51/maintenance/upload-cache-to-github.py'
 script_name='upload-cache-to-github.py'
 
+pip='pip'
+if [ type pip3 ]; then
+  pip='pip3'
+fi
+"$pip" install --user requests
+
 curl -s "$hunter_upload_script_url" > "$script_name"
 chmod +x "$script_name"
 ./"$script_name" --username hunter-cache-bot --repo-owner ethereum --repo hunter-cache --cache-dir ~/.hunter/_Base/Cache --temp-dir ~/.hunter/tmp
