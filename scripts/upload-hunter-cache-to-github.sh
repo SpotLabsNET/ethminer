c229722d110b61c156f872c5ac681d0a054baa90
Paweł Bylica <chfast@gmail.com>
2017-07-26 18:35:09 +0200

    Install python3 on OSX

--- scripts/upload-hunter-cache-to-github.sh
+++ scripts/upload-hunter-cache-to-github.sh
@@ -3,16 +3,11 @@
 hunter_upload_script_url='https://raw.githubusercontent.com/ruslo/hunter/86b488030412a12c9a950977bf3d83b6fbcb7f51/maintenance/upload-cache-to-github.py'
 script_name='upload-cache-to-github.py'
 
-python='python'
-if type python3; then
-  python='python3'
-fi
-pip='pip'
-if type pip3; then
-  pip='pip3'
+if [ "$OSTYPE" = "darwin"* ]; then
+  brew install python3
 fi
 
-"$pip" install --user requests
+pip3 install --user requests
 
 curl -s "$hunter_upload_script_url" > "$script_name"
-$python "$script_name" --username hunter-cache-bot --repo-owner ethereum --repo hunter-cache --cache-dir ~/.hunter/_Base/Cache --temp-dir ~/.hunter/tmp
+python3 "$script_name" --username hunter-cache-bot --repo-owner ethereum --repo hunter-cache --cache-dir ~/.hunter/_Base/Cache --temp-dir ~/.hunter/tmp
